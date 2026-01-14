958fef592c4a0ce04c34cc7ad69e054d9be5b6c6
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-14 23:22:59 +0530

    feat: print 1 to N 2 methods notes

---
+++ Basics/print1toN.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//1) For-loop
+//2) Recursion : Head recursion and tail recursion
+// tail me recursive call end me lagta hai and processing upar
+
+/*
+Tail : Processing
+       Recursive call(fn)
+
+Head : Recursive call(fn)
+       Processing
+*/
+
+int main(){
+    
+    
+
+    return 0;
+}
