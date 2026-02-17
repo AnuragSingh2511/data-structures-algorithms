0d834873089a6b2737d165ff13b71a56430680ea
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-02-17 22:01:49 +0530

    feat: full code 1D to 2D array

--- Leetcode/1DTo2D.cpp
+++ Leetcode/1DTo2D.cpp
@@ -10,3 +10,62 @@ x = c*i + j
 row = x/c
 col = x%c
 */
+
+// TC: O(m * n)
+// We traverse the original array once
+
+// SC: O(m * n)
+// We create a 2D vector of size m × n
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+
+    if(m * n != original.size()){
+        return {};
+    }
+
+    vector<vector<int>> res(m, vector<int>(n));
+
+    for(int i = 0; i < original.size(); i++){
+        res[i / n][i % n] = original[i];
+    }
+
+    return res;
+}
+
+int main() {
+
+    /*
+    Sample Input:
+    original = [1,2,3,4,5,6]
+    m = 2
+    n = 3
+
+    Sample Output:
+    1 2 3
+    4 5 6
+    */
+
+    vector<int> original = {1,2,3,4,5,6};
+    int m = 2;
+    int n = 3;
+
+    vector<vector<int>> result = construct2DArray(original, m, n);
+
+    if(result.empty()){
+        cout << "Invalid dimensions";
+        return 0;
+    }
+
+    for(auto &row : result){
+        for(auto &val : row){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
